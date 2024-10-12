3600f165e5d6a0a6b2fc1f54ae77d6502e39cfed
danishkodemonkey <humberjini@hotmail.com>
2024-10-12 20:59:12 +0200

    add google font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: { fontFamily: { sans: ['Poppins', 'sans-serif'] } },
     },
     plugins: [],
 };
