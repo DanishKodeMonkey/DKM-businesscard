10afe00ca98857ee2b5044426f4196c69159f323
danishkodemonkey <humberjini@hotmail.com>
2024-10-15 16:11:18 +0200

    update webpack config to hash js file for github updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: { index: './src/index.ts' },
     output: {
-        filename: 'index.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
